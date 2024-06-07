8f9bf85b492a626fcb9bd6a4de0723ec51a983d8
Victor Balbo <victorbalbo@gmail.com>
2024-06-06 21:04:13 -0300

    Create base layout

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
@@ -14,65 +15,32 @@
 
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.app {
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.main {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  max-width: 45rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.aside {
+  height: 100%;
+  position: relative;
+  flex-grow: 1;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.map-component:before {
+  position: absolute;
+  display: block;
+  box-shadow: inset 1.25rem 0 1rem -1rem rgba(0, 0, 0, 0.5);
+  content: '';
+  z-index: 1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  pointer-events: none;
 }
 </style>
---
+++ src/views/PlanView.vue
@@ -0,0 +1,3 @@
+<script setup lang="ts"></script>
+<template>HEADER BODY</template>
+<style scoped></style>
