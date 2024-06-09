3ce9ba276db6d1894375e04d9f845445462ee89d
Victor Balbo <victorbalbo@gmail.com>
2024-06-09 20:42:53 -0300

    create button and tag component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class="button">
+    <slot />
+  </article>
+</template>
+<style scoped>
+.button {
+  width: fit-content;
+  padding: var(--small-spacing);
+  border-radius: var(--small-spacing);
+  background-color: var(--color-primary);
+  color: white;
+  cursor: pointer;
+  transition: var(--default-transition);
+}
+.button:hover {
+  background-color: var(--color-blue-faded);
+}
+.button:active {
+  background-color: var(--color-blue-active);
+}
+</style>
---
+++ src/components/TagComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ value: string }>()
+</script>
+
+<template>
+  <p class="tag">{{ value }}</p>
+</template>
+
+<style scoped>
+.tag {
+  background-color: var(--color-light-gray);
+  border-radius: var(--small-spacing);
+  display: inline-block;
+  padding: 0 var(--small-spacing);
+}
+</style>
