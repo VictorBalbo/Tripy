49929a985a2a46f950e987fbac0f379c852a3f2b
Victor Balbo <victorbalbo@gmail.com>
2024-07-16 21:25:24 -0300

    Add new icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,21 +1,27 @@
 import AddIcon from '@/components/icons/AddIcon.vue'
+import BedIcon from '@/components/icons/BedIcon.vue'
 import ClockIcon from '@/components/icons/ClockIcon.vue'
 import EditIcon from '@/components/icons/EditIcon.vue'
 import GlobeIcon from '@/components/icons/GlobeIcon.vue'
 import MarkerIcon from '@/components/icons/MarkerIcon.vue'
 import MinusIcon from '@/components/icons/MinusIcon.vue'
+import MoonIcon from '@/components/icons/MoonIcon.vue'
 import PhoneIcon from '@/components/icons/PhoneIcon.vue'
 import StarIcon from '@/components/icons/StarIcon.vue'
+import TicketIcon from '@/components/icons/TicketIcon.vue'
 import TrashIcon from '@/components/icons/TrashIcon.vue'
 
 export {
   AddIcon,
+  BedIcon,
   ClockIcon,
   EditIcon,
   GlobeIcon,
   MarkerIcon,
   MinusIcon,
+  MoonIcon,
   StarIcon,
   PhoneIcon,
+  TicketIcon,
   TrashIcon
 }
